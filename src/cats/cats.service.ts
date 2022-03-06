a4f3aa83e792236c6e6633cfcd07773d7223f00c
5witchkr <hob4410@gmail.com>
2022-03-06 16:27:47 +0900

    nestTypeORM-Mysql

---
+++ src/cats/cats.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { getConnection, Repository } from 'typeorm';
+import { Cat } from './entity/cats.entity';
+
+
+@Injectable()
+export class CatsService {
+    constructor(
+        @InjectRepository(Cat)
+        private catsRepository: Repository<Cat>,
+    ){}
+
+    findAll(): Promise<Cat[]> {
+        return this.catsRepository.find();
+    }
+
+    findOne(id: number): Promise<Cat> {
+        return this.catsRepository.findOne(id);
+    }
+
+    async create(cat: Cat): Promise<void> {
+        await this.catsRepository.save(cat);
+    }
+
+    async remove(id: number): Promise<void> {
+        await this.catsRepository.delete(id);
+    }
+
+    async update(id: number, cat: Cat): Promise<void> {
+        const existCat = await this.catsRepository.findOne(id);
+        if(existCat) {
+            await getConnection()
+                .createQueryBuilder()
+                .update(Cat)
+                .set({
+                    name: cat.name,
+                    age: cat.age,
+                    breed: cat.breed
+                })
+                .where("id = :id", {id})
+                .execute();
+        }
+    }
+}
