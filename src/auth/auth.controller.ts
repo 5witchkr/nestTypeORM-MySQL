0f5e2d42dec024ce6a4e8a0da511291aca558980
5witchkr <hob4419@gmail.com>
2022-03-14 01:52:28 +0900

    cookie&logout

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -21,7 +21,14 @@ export class AuthController {
     async login(@Body() userDTO: UserDTO, @Res() res: Response): Promise<any> {
         const jwt = await this.authService.validateUser(userDTO);
         res.setHeader('Authorization', 'Bearer '+jwt.accessToken);
-        return res.json(jwt);
+        //쿠키로 전달 httpOnly는 브라우저에서 cookie를 이용할수없게만들어서 xss등의 보안을 강화
+        res.cookie('jwt', jwt.accessToken, {
+            httpOnly: true,
+            maxAge: 2 * 60 * 60 * 1000
+        });
+        return res.send({
+            message: 'success'
+        });
     }
     //jwt토큰 인증 req.user값을 읽어오려면 authguard로 가서 검증
     @Get('authenticate')
@@ -37,5 +44,24 @@ export class AuthController {
     adminRoleCheck(@Req() req: Request): any {
         const user: any = req.user;
         return user;
+    }
+
+    @Post('logout')
+    logout(@Res() res: Response): any {
+        res.cookie('jwt', '', {
+            maxAge: 0
+        });
+        return res.send({
+            message: 'success'
+        })
+    }
+
+    //cookie테스트
+    @Get('cookies')
+    getCookies(@Req() req: Request, @Res() res: Response): any {
+        const jwt = req.cookies['jwt'];
+        return res.send({
+            message: 'cookie'
+        });
     }
 }
